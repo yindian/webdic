0c8382bd1f98313a5108298ae478845fd6f8bfe9
yindian <yindian@c827c7b3-856e-083b-50f2-449c6c71a728>
2011-02-17 02:12:31 +0000

    smaller file size for logo

--- trunk/webdic/static/logo.png.cmd
+++ trunk/webdic/static/logo.png.cmd
@@ -1,2 +1,4 @@
 convert -size 320x85 xc:transparent -font Arial-Bold-Italic -pointsize 72 -draw "text 25,70 'WebDic'" -channel RGBA -gaussian 0x6 -fill royalblue -stroke lightblue -draw "text 20,65 'WebDic'" -depth 8 logo.png
+pngnq -n 256 logo.png
+move /y logo-nq8.png logo.png
 pngout logo.png
