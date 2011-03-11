bd25772923b7317d2cdaa5cbab805a3311a5a3bc
yindian <yindian@c827c7b3-856e-083b-50f2-449c6c71a728>
2011-03-11 15:55:34 +0000

    stardict engine load cache

--- trunk/webdic/diceng/stardict.py
+++ trunk/webdic/diceng/stardict.py
@@ -27,7 +27,8 @@ License (MIT)
 
 '''
 import diceng
-import os.path
+import os, string, struct, re, gzip, dictzip
+import logging, traceback
 
 def parseifo(path):
 	f = open(path, 'r')
@@ -48,6 +49,28 @@ def parseifo(path):
 	finally:
 		f.close()
 
+def getcachepath(basename, ext):
+	return os.path.join(diceng.getcachedir(), basename + ext)
+
+def hascache(root, basename, ext, size=0):
+	path = getcachepath(basename, ext)
+	if not os.path.exists(path):
+		return False
+	try:
+		st = os.stat(path)
+		if size and st.st_size != size:
+			return False
+		if st.st_mtime < os.stat(root + '.ifo').st_mtime:
+			return False
+	except:
+		logging.error(traceback.format_exc())
+		return False
+	return True
+
+idxentrypat = re.compile(r'(.*?)\x00.{8}', re.S)
+synentrypat = re.compile(r'(.*?)\x00.{4}', re.S)
+lower = string.lower
+
 class StardictEngine(diceng.BaseDictionaryEngine):
 	@staticmethod
 	def _getbasename(path):
@@ -57,13 +80,75 @@ class StardictEngine(diceng.BaseDictionaryEngine):
 				parseifo(path)
 				return os.path.basename(root)
 			except:
+				logging.error(traceback.format_exc())
 				pass
 	def _load(self):
 		print 'loading', self._basename
 		self._name, self._sametypeseq, idxsize, self._wordcnt, self._syncnt, d\
 				= parseifo(self._path)
+		root, ext = os.path.splitext(self._path)
+		try:
+			self._idxf = open(root + '.idx', 'rb')
+		except IOError:
+			self._idxf = gzip.GzipFile(root + '.idx.gz', 'rb')
+		if hascache(root, self._basename, '.pos', size=4*self._wordcnt):
+			f = open(getcachepath(self._basename, '.pos'), 'rb')
+			buf = f.read()
+			f.close()
+			self._indices = struct.unpack('<%dL' % (self._wordcnt,), buf)
+			indices = None
+		else:
+			buf = self._idxf.read()
+			assert self._idxf.tell() == idxsize
+			indices = [] #(collated word, entry offset)
+			pos = 0
+			for s in idxentrypat.findall(buf):
+				indices.append((lower(s.decode('utf-8')), pos))
+				pos += len(s) + 9
+			assert pos == idxsize
+			indices.sort()
+			self._indices = [p for s, p in indices]
+			f = open(getcachepath(self._basename, '.pos'), 'wb')
+			f.write(struct.pack('<%dL' % (self._wordcnt,), *self._indices))
+			f.close()
+		assert len(self._indices) == self._wordcnt
+		if self._syncnt > 0:
+			try:
+				self._synf = open(root + '.syn', 'rb')
+			except IOError:
+				self._synf = gzip.GzipFile(root + '.syn.gz', 'rb')
+			if hascache(root, self._basename, '.spo', size=4*(self._wordcnt +
+				self._syncnt)):
+				f = open(getcachepath(self._basename, '.spo'), 'rb')
+				buf = f.read()
+				f.close()
+				self._synindices = struct.unpack('<%dL' % (len(buf)/4,), buf)
+			else:
+				if indices is None:
+					buf = self._idxf.read()
+					assert self._idxf.tell() == idxsize
+					indices = [] #(collated word, entry offset)
+					pos = 0
+					for s in idxentrypat.findall(buf):
+						indices.append((lower(s.decode('utf-8')), pos))
+						pos += len(s) + 9
+					assert pos == idxsize
+				buf = self._synf.read()
+				pos = 0
+				for s in synentrypat.findall(buf):
+					indices.append((lower(s.decode('utf-8')), pos & 0x8000000))
+					pos += len(s) + 5
+				assert len(indices) ==  self._wordcnt + self._syncnt
+				indices.sort()
+				self._synindices = [p for s, p in indices]
+				f = open(getcachepath(self._basename, '.spo'), 'wb')
+				f.write(struct.pack('<%dL' % (len(indices),),*self._synindices))
+				f.close()
+		else:
+			self._synf = self._synindices = None
 	def _query(self, qstr, qtype=None, qparam=None):
-		pass
+		if qtype is None or qtype in (diceng.QRY_EXACT, diceng.QRY_BEGIN):
+			pass
 
 def register():
 	print 'Register Stardict'
